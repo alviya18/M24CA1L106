e36999c1e563f7aef60768f8200a6aa9a494167c
alviya18 <136180312+alviya18@users.noreply.github.com>
2025-10-13 19:08:27 +0530

    Update exp30.py

--- exp30.py
+++ exp30.py
@@ -20,7 +20,7 @@ def sumOfList(list):
     return list[0] + sumOfList(list[1:])
 
 def numberSum(a):
-    return a + numberSum(a-1) if a >= 1 else 0
+    return a-1 + numberSum(a-1) if a >= 1 else 0
 
 def stringReverse(string):
     if string == "":
@@ -32,3 +32,4 @@ print(f"7! is {factorial(7)}"
       f"\nSum of the list 1,45,48,74,2,24,9 is {sumOfList([1,45,48,74,2,24,9])} "
       f"\nSum of first 45 whole numbers is {numberSum(45)}"
       f"\nHello word reversed is {stringReverse('hello world')}")
+
