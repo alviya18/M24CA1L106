bc4931143985549993298a56227b83e99447c8e4
alviya18 <136180312+alviya18@users.noreply.github.com>
2025-10-13 18:27:12 +0530

    Update exp30.py

--- exp30.py
+++ exp30.py
@@ -15,17 +15,20 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2)
 
 def sumOfList(list):
+    if len(list) == 0:
+        return 0
     return list[0] + sumOfList(list[1:])
 
 def numberSum(a):
-    return a + numberSum(a-1) if a > 1 else ""
+    return a + numberSum(a-1) if a >= 1 else 0
 
 def stringReverse(string):
-    if not string == "":
-        return string[-1]+string[:-1]
+    if string == "":
+        return ""
+    return string[-1] + stringReverse(string[:-1])
 
-print(f"Factorial of 5 is {factorial(7)}"
+print(f"7! is {factorial(7)}"
       f"\nThe 10th number in fibonacci series is {fibonacci(10)}"
       f"\nSum of the list 1,45,48,74,2,24,9 is {sumOfList([1,45,48,74,2,24,9])} "
       f"\nSum of first 45 whole numbers is {numberSum(45)}"
-      f"\nHello word reversed is {stringReverse("hello world")}")
+      f"\nHello word reversed is {stringReverse('hello world')}")
